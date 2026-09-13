160efa6a81d2d400aeb8ec9756e97e995bc1f264
ahmad123-567 <ahmadjutt07795@gmail.com>
2026-09-13 14:44:48 +0500

    Refactor rag_utils.py for improved functionality

--- rag_utils.py
+++ rag_utils.py
@@ -3,7 +3,7 @@ import glob
 import json
 import streamlit as st
 from langchain_community.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from groq import Groq
